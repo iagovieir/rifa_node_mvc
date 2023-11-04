512dcfca083de603b70a537ab02221d701a90a84
Iago Vieira <iago.unijp@gmail.com>
2023-11-04 16:30:44 -0300

    gerando contagem por pessoa

--- src/controllers/NumbersController.js
+++ src/controllers/NumbersController.js
@@ -39,12 +39,78 @@ module.exports = {
         const listar = mongoose.model('list')
         const lista = await listar.find({}).lean()
 
+        let count = {
+        Iago: 0,
+        Leticia:  0,
+        Riguad: 0,
+        Gilsinho: 0,
+        Mayara: 0,
+        Mariana: 0,
+        Brasil: 0,
+        Rafael: 0,
+        Gustavo: 0,
+        EricaFrazao: 0,
+        Gabriela: 0,
+        Bebesso: 0,
+        Lisandra: 0,
+        Magna: 0,
+        Maria: 0
+        }
+        lista.forEach(lista=>{
+            if(lista.From == 'Iago'){
+                count.Iago += 1 
+            }
+            if(lista.From == 'Leticia'){
+                count.Leticia += 1 
+            }
+            if(lista.From == 'Riguad'){
+                count.Riguad += 1 
+            }
+            if(lista.From == 'Gilsinho'){
+                count.Gilsinho += 1 
+            }
+            if(lista.From == 'Mayara'){
+                count.Mayara += 1 
+            }
+            if(lista.From == 'Mariana'){
+                count.Mariana += 1 
+            }
+            if(lista.From == 'Brasil'){
+                count.Brasil += 1 
+            }
+            if(lista.From == 'Rafael'){
+                count.Rafael += 1 
+            }
+            if(lista.From == 'Gustavo'){
+                count.Gustavo += 1 
+            }
+            if(lista.From == 'Érica Frazão'){
+                count.EricaFrazao += 1 
+            }
+            if(lista.From == 'Gabriela'){
+                count.Gabriela += 1 
+            }
+            if(lista.From == 'Bebesso'){
+                count.Bebesso += 1 
+            }
+            if(lista.From == 'Lisandra'){
+                count.Lisandra += 1 
+            }
+            if(lista.From == 'Magna'){
+                count.Magna += 1 
+            }
+            if(lista.From == 'Maria'){
+                count.Maria += 1 
+            }
+        })
+
         res.render('list',{
             title: 'Comprar - EJC',
             style: 'list.css',
             script: 'list.js',
             listar: lista,
-            countList: lista.length
+            countList: lista.length,
+            count: count,
 
         })
     }
